987701953d6a8f2e327bb86818b64ed28bcbfd78
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-02-15 15:43:47 +0700

    add a order form

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,4 +1,10 @@
 class FoodItem < ApplicationRecord
   belongs_to :section
   validates :name, :price, presence: true
+  has_many :orders
+  
+  def image_url_or_default
+    image_url.presence || "http://loremflickr.com/320/240/#{name.gsub(' ','')}"  ##returns null if there is no url and in that case uses loremflickr
+    
+  end
 end
