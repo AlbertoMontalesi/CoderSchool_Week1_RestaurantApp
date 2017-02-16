fdcf139978aaaa596ca779574343ca70717308fa
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-02-16 17:00:34 +0700

    fixed stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,15 @@ class HomeController < ApplicationController
     @sections = Section.all
     if params[:section_id].present?
       @current_section = Section.find(params[:section_id])
-  end
-  end
+end
+
+  if params[:sort_column]
+    @food_items = @food_items.order("#{params[:section]} #{params[:sort_column]} #{params[:sort_direction]}")
+    ## fix the code to make the sort function work
+
+  end 
+
+  
+
+end
 end
