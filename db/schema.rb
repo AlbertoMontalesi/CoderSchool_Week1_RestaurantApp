9b67540bcc71291d77a88c035d3b9d1bfc8bc7e3
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-02-18 18:42:51 +0700

    finished order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218113641) do
+ActiveRecord::Schema.define(version: 20170218114117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170218113641) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "name"
-    t.integer  "phone"
+    t.bigint   "phone"
     t.string   "surname"
     t.string   "address"
     t.index ["food_item_id"], name: "index_orders_on_food_item_id", using: :btree
